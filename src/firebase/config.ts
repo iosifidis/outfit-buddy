d7848629e43fee1e73fd911920f384462f776b50
Efstathios Iosifidis, DVM <eiosifidis@gmail.com>
2025-12-08 19:07:34 +0200

    Update Firebase API key to placeholder
    Replaced the Firebase API key with a placeholder.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-2998395971-59cfc",
   "appId": "1:807768049700:web:2b164ea30717d195c870b1",
-  "apiKey": "AIzaSyDBXoTQv1ZqiqAS8o6i0X8oCB5fOgUPXTY",
+  "apiKey": "XXXXXXXXXXXXX",
   "authDomain": "studio-2998395971-59cfc.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "807768049700"
